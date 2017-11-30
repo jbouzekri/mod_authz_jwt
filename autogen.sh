b2090f5df537fab8a5be857971f19bc6fd60c01d
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2017-11-30 15:38:52 +0100

    configure automake install

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "Cleanup"
-rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log config.status config.guess config.sub .deps stamp-h1 depcomp install-sh ltmain.sh missing libtool config.h config.h.in config.h.in~ m4 ar-lib compile
+rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log
+rm -rf config.status config.guess config.sub .deps stamp-h1 depcomp
+rm -rf install-sh ltmain.sh missing libtool config.h config.h.in config.h.in~
+rm -rf m4 ar-lib compile authz_jwt.load
 rm -rf *.o *.la *.lo *.slo Makefile.in Makefile
 
 if [ "$1" = "clean" ] ; then
